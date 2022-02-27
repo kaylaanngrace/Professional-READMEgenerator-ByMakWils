58e82f7458a9431b2f6e8ccd26915526516d0e4b
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-02-27 00:15:25 -0600

    added dist folder. added packages to index.js and added writefile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,30 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileContent, err => {
+            // if there's an error reject promise
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            // file created if no errors
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {}
