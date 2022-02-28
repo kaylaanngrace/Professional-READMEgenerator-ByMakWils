dfb94068f8c2198b648a6ca70c98f9584b86736d
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-02-27 21:13:02 -0600

    added license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,15 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license = !'No license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+    `
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
